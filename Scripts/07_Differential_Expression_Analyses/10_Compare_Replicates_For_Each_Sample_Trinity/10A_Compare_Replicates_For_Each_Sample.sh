16bee1bd60ae5d0599bcf14939dd08e49eebc4c1
mjbieren <107464589+mjbieren@users.noreply.github.com>
2024-06-20 19:24:04 +0200

    Create 10A_Compare_Replicates_For_Each_Sample.sh

---
+++ Scripts/07_Differential_Expression_Analyses/10_Compare_Replicates_For_Each_Sample_Trinity/10A_Compare_Replicates_For_Each_Sample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#If you use a singularity command be sure to change all your full paths to mounted as a start path since you have to mount your directory. Change the path accordingly. In this case /data/ is turned into /mounted/
+
+SINGULARITY_HOME_PATH=${PWD}
+
+MATRIX=/home/Filtered_1_ALL_Only_abundance_estimates_matrix.isoform.counts.matrix
+SAMPLE_LIST=/home/Biological_Replicates.txt
+OUTPUT_PREFIX=Filtered_01_ALL_Only_A_
+SINGULARITY_TRINITY_IMAGE=/home/Singularity_Images/Trinity/trinityrnaseq.v2.15.1.simg
+
+#Testing
+#singularity exec --bind /data:/mounted $SINGULARITY_TRINITY_IMAGE ls /mounted
+
+#To remove the localisation error for perl
+export LC_ALL=C
+
+#Compare replicates for each of your samples
+singularity exec --home $SINGULARITY_HOME_PATH:/home --bind /data:/mounted $SINGULARITY_TRINITY_IMAGE  /usr/local/bin/Analysis/DifferentialExpression/PtR --matrix $MATRIX --samples $SAMPLE_LIST --min_rowSums 10 --compare_replicates --save --output $OUTPUT_PREFIX
